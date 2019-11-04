85e71850b32218bbf1f0f4659a57fb9357b1b7e2
Zane Waite <zmwaite182@stevenscollege.edu>
2019-11-04 09:32:24 -0500

    Add index, login, and register page, Add login form

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+
+    <form method="post" action="login.php">
+        <input type="submit" name="login" value="Login">
+    </form>
+    <form method="post" action="register.php">
+        <input type="submit" name="register" value="Register">
+    </form>
+    
+</body>
+</html>
