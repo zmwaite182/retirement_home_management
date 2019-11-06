e8a1a181a9a2e451f7d8a78dc60a9a5c1b388e88
JonathanWitman <jawitman8@gmail.com>
2019-11-06 09:17:55 -0500

    Establishes connection to to database in order to insert and confirm new account creation. It also presents additional information if said user signs up as a patient. Update schema for additional reliability. Introduces db file for database connection.

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+  $dbServername = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbName = "retirement_home";
+
+  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+ ?>
--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+  include_once 'db.php';
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +12,70 @@
     <title>Document</title>
 </head>
 <body>
-    <p>REGISTER</p>
+
+  <form action="" method="post">
+    <select name="role_selection">
+      <option value="none">Select Role</option>
+      <option value="patient">Patient</option>
+      <option value="family_member">Family Member</option>
+      <option value="supervisor">Supervisor</option>
+      <option value="caregiver">Care Giver</option>
+      <option value="doctor">Doctor</option>
+    </select>
+
+    <input type="text" name="f_name" placeholder="First Name">
+    <input type="text" name="l_name" placeholder="Last Name">
+    <input type="email" name="email" placeholder="Email">
+    <input type="tel" name="phone" placeholder="Phone Number">
+    <input type="text" name="password" placeholder="Password">
+    <input type="date" name="birth" placeholder="Date of Birth">
+    <input type="submit" name="create_acc" value="Submit"/>
+  </form>
+
+  <?php
+
+    if (isset($_POST['create_acc'])) {
+      $f_name = $_POST['f_name'];
+      $l_name = $_POST['l_name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $password = $_POST['password'];
+      $birth = $_POST['birth'];
+      $role = $_POST['role_selection'];
+
+      $sql = "INSERT INTO `users` (job, f_name, l_name, email, phone, user_password, dob) VALUES ('$role', '$f_name', '$l_name', '$email', '$phone', '$password', '$birth');";
+      mysqli_query($conn, $sql);
+
+      //further optimize selection in the future
+      $get_id = "SELECT user_id FROM users WHERE email = '$email';";
+      $user_id= mysqli_query($conn, $get_id);
+      while($row = mysqli_fetch_assoc($user_id)) {
+        foreach ($row as $value) {
+          $_SESSION['user_id'] = $value;
+        }
+      }
+
+      if ($role == "patient") {
+        echo
+        "<form action='' method='post'>
+          <input type='text' name='fam_code' placeholder='Family Code'>
+          <input type='text' name='emergency_contact' placeholder='Emergency Contact'>
+          <input type='text' name='relation' placeholder='Relation to Contact'>
+          <input type='submit' name='add_info' value='Submit'/>
+        </form>";
+      }
+    }
+    if (isset($_POST['add_info'])) {
+      $fam_code = $_POST['fam_code'];
+      $emergency_contact = $_POST['emergency_contact'];
+      $relation = $_POST['relation'];
+      $user_id = $_SESSION['user_id'];
+
+      $sql = "INSERT INTO `patients` (user_id, family_code, emergency_contact, relation_ec) VALUES ('$user_id', '$fam_code', '$emergency_contact', '$relation');";
+      mysqli_query($conn, $sql);
+    }
+  ?>
+
+
 </body>
 </html>
