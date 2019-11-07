719d0a2b3598147cb63f14206c7f47837777e5a3
Zane Waite <zmwaite182@stevenscollege.edu>
2019-11-07 08:54:26 -0500

    Start session from login and index page, Complete index page for user not logged in, Allow user to login

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php 
+    include_once 'db.php';
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +17,27 @@
         <input type="text" name="email" required>
         <label for="password">Password</label>
         <input type="text" name="password" required>
-        <input type="submit" name="login">
+        <input type="submit" name="grant_access">
     </form>
-    <form action="index.php" method="post">
-        <input type="submit" value="Cancel">
-    </form>
+
+    <?php
+        if (isset($_POST['grant_access'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $get_pass = "SELECT user_password, job, user_id from users WHERE email = '$email';";
+            $user_pass= mysqli_query($conn, $get_pass);
+            while($row = mysqli_fetch_assoc($user_pass)) {
+                $correct_password = $row['user_password'];
+                $job = $row['job'];
+                $user_id = $row['user_id'];
+            }
+            if ($password = $correct_password) {
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['job'] = $job;
+            }
+        } 
+    ?>
+
+    <a href="./index.php">Cancel</a>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,6 +1,5 @@
 <?php
   include_once 'db.php';
-  session_start();
 ?>
 
 <!DOCTYPE html>
